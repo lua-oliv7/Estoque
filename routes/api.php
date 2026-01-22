4131924224e999b6191e20674136bad58c3e961c
lua-oliv7 <luana.a.oliveira7@hotmail.com>
2026-01-22 12:43:31 -0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Route::post('jogadores', [JogadorController::class, 'store']);
+Route::get('jogadores', [JogadorController::class, 'index']);
+Route::put('jogadores', [JogadorController::class, 'update']);
+Route::delete('jogadores/{id}', [JogadorController::class, 'delete']);
+Route::get('jogadores/{id}', [JogadorController::class, 'show']);
+*/
